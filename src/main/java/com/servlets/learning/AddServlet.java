2ca2363733ebd2309f5e656e316b69472175b5f7
karan2514 <karan2514singh@gmail.com>
2024-02-23 19:02:20 +0530

    servlet context

--- src/main/java/com/servlets/learning/AddServlet.java
+++ src/main/java/com/servlets/learning/AddServlet.java
@@ -1,32 +1,25 @@
 package com.servlets.learning;
 
 import java.io.IOException;
-import jakarta.servlet.RequestDispatcher;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class AddServlet extends HttpServlet{
 	
-	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
-		int i = Integer.parseInt(req.getParameter("num1"));
-		int j = Integer.parseInt(req.getParameter("num2"));
-		int sum = i+j;
-		req.setAttribute("sum", sum);
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
 		
-		Cookie cookie = new Cookie("sum", sum +"");
-		res.addCookie(cookie);
-		res.sendRedirect("sq");
+		PrintWriter out = res.getWriter();
+		out.println("Hi<br>");
 		
-		/*
-		 * PrintWriter out = res.getWriter(); out.println("sum is: "+sum);
-		 */
+		ServletContext ctx = getServletContext();
 		
-		/*
-		 * RequestDispatcher rd = req.getRequestDispatcher("sq"); rd.forward(req, res);
-		 */
+		String str = ctx.getInitParameter("name");
+		out.println(str);
 	}
 
 }
